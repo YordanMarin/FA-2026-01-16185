73d06d2e44c7c70c02ede5d5982f2a2c02c1eb33
yordan <yordan@gmail.com>
2026-04-09 15:38:45 -0500

    subiendo ejer 1 en c#

---
+++ SE3-C/ejer1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class ejer1
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine("\n----- IMPRESO CON + ----");
+            Console.WriteLine(nombre+", Bienvenido al curso de Fundamentos de Algoritmos de la carrera "+carrera);
+
+            Console.WriteLine("\n----- IMPRESO CON $ ----");
+            Console.WriteLine($"{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}");
+            Console.ReadKey();
+        }
+    }
+}
