076f1d19f037d49b71a51c4d283d588c7176c51a
yordan <yordan@gmail.com>
2026-04-16 15:55:46 -0500

    actualizando ejer3

---
+++ SE3-P/ejer3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingrese número 1: "))
+num2 = int(input("Ingrese número 2: "))
+
+print("\nSuma: ",(num1+num2))
+print("Resta: " , (num1 - num2))
+print("Multiplicación: " , (num1 * num2))
+print("División: " , (num1 / num2))
